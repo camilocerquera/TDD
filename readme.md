a1d51f529765ffae6267000c0966946d3ff33018
camilocerquera <jc.cerquera10@uniandes.edu.co>
2015-09-19 15:37:06 -0500

    Paso 2: 1. (Rojo) un número.

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Paso 1
 1. (Rojo) cadena vacia.
 2. (Verde) cadena vacia.
+
+# Paso 2
+1. (Rojo) un número
--- test_calculadora.py
+++ test_calculadora.py
@@ -10,3 +10,7 @@ class TestCalculadora(TestCase):
 
     def test_sumar_cadenaVacia(self):
         self.assertEqual(Calculadora().sumar(""), 0, "cadena vacia")
+
+    def test_sumar_unNumero(self):
+        numero = "1"
+        self.assertEqual(Calculadora().sumar(numero), numero, "un numero")
